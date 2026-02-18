d15ceb875e46f73c4daeda09f6006ec028617a8c
Faisal Abid <ifaisalabid1@gmail.com>
2026-02-18 12:03:44 +0530

    Inject dependencies in main file

--- Makefile
+++ Makefile
@@ -5,3 +5,13 @@ migrate-up:
 
 migrate-down:
 	migrate -path ./migrations -database "postgres://$(DB_USER):$(DB_PASSWORD)@$(DB_HOST):$(DB_PORT)/$(DB_NAME)?sslmode=$(DB_SSLMODE)" down
+
+build:
+	go build -o bin/api ./cmd/api
+
+run:
+	go run ./cmd/api/main.go
+
+clean:
+	rm -rf bin/
+	go clean -cache
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,11 +5,16 @@ import (
 	"database/sql"
 	"fmt"
 	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/ifaisalabid1/url-shortener/internal/config"
+	"github.com/ifaisalabid1/url-shortener/internal/handler"
 	"github.com/ifaisalabid1/url-shortener/internal/repository"
+	"github.com/ifaisalabid1/url-shortener/internal/service"
 	_ "github.com/lib/pq"
 	"github.com/redis/go-redis/v9"
 )
@@ -46,6 +51,43 @@ func main() {
 
 	urlRepo := repository.NewURLRepository(db)
 	cacheRepo := repository.NewClientRepository(redisClient)
+	urlService := service.NewURLService(urlRepo, cacheRepo, cfg.App.BaseURL, cfg.App.ShortLength, cfg.App.CacheTTL)
+	urlHandler := handler.NewURLHandler(urlService, logger)
+	router := handler.Routes(urlHandler, logger)
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%s", cfg.Server.Port),
+		Handler:      router,
+		ReadTimeout:  cfg.Server.ReadTimeout,
+		WriteTimeout: cfg.Server.WriteTimeout,
+		IdleTimeout:  cfg.Server.IdleTimeout,
+	}
+
+	go func() {
+		logger.Info("starting server", "port", cfg.Postgres.Port)
+		err := server.ListenAndServe()
+		logger.Error("server failed to start", "error", err)
+		os.Exit(1)
+	}()
+
+	go startCleanupJob(db, logger)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	logger.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Error("server forced to shutdown", "error", err)
+		os.Exit(1)
+	}
+
+	logger.Info("server stopped")
+
 }
 
 func connectDB(cfg *config.Config) (*sql.DB, error) {
@@ -89,4 +131,25 @@ func connectRedis(cfg *config.Config) (*redis.Client, error) {
 	}
 
 	return client, nil
+}
+
+func startCleanupJob(db *sql.DB, logger *slog.Logger) {
+	ticker := time.NewTicker(24 * time.Hour)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		logger.Info("Running cleanup job for expired urls")
+
+		query := "DELETE FROM urls WHERE expires_at <= NOW()"
+
+		result, err := db.Exec(query)
+		if err != nil {
+			logger.Error("failed to clean expired urls", "error", err)
+			continue
+		}
+
+		rows, _ := result.RowsAffected()
+
+		logger.Info("Cleanup job completed", "deleted rows", rows)
+	}
 }
